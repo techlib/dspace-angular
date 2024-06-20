bb53d1b5f02321232d81bbc0c933c3e1ca2c766d
Repository Administrator <repozitar@techlib.cz>
2024-06-20 18:51:47 +0200

    custom language packs and basic Simple Item page edits
    custom language packs and hacked together Simple Item page for PIDfest aftermath - done directly on base theme. Also edited the login applet label to 'username' in our custom langpacks

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
-// import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
-
+//import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module'// import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 /**
  * This module bundles the eager theme modules for all available themes.
  * Eager modules contain components that are present on every page (to speed up initial loading)
@@ -11,8 +10,8 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
-    // CustomEagerThemeModule,
+	  //DSpaceEagerThemeModule,
+    CustomEagerThemeModule,
   ],
 })
 export class EagerThemesModule {
