ce856e7e70809d16ad4d1ccdad3c5a76d1547154
Repository Administrator <repozitar@techlib.cz>
2025-01-16 11:50:17 +0100

    customised Publication Simple Item from 7.6

--- src/themes/custom/app/item-page/simple/item-types/publication/publication.component.ts
+++ src/themes/custom/app/item-page/simple/item-types/publication/publication.component.ts
@@ -14,7 +14,7 @@ import { PublicationComponent as BaseComponent } from '../../../../../../../app/
   // styleUrls: ['./publication.component.scss'],
   styleUrls: ['../../../../../../../app/item-page/simple/item-types/publication/publication.component.scss'],
   templateUrl: './publication.component.html',
-  // templateUrl: '../../../../../../../app/item-page/simple/item-types/publication/publication.component.html',
+  //templateUrl: '../../../../../../../app/item-page/simple/item-types/publication/publication.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PublicationComponent extends BaseComponent {
