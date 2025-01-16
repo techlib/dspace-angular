9fa6ed0028c778af6d354a53b4779c6e3507aa1f
Repository Administrator <repozitar@techlib.cz>
2025-01-16 11:51:22 +0100

    edited eager-themes.module.ts to properly register custom theme and properly load themeable components from the theme folder

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
-// import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
+import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
  * This module bundles the eager theme modules for all available themes.
@@ -12,7 +12,7 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
 @NgModule({
   imports: [
     DSpaceEagerThemeModule,
-    // CustomEagerThemeModule,
+    CustomEagerThemeModule,
   ],
 })
 export class EagerThemesModule {
