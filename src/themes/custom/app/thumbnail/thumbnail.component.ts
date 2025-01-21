6ef7b10f785c0a5a03d63deb8d45c80e2019cfe1
Repository Administrator <repozitar@techlib.cz>
2025-01-21 22:17:47 +0100

    custom Thumbnail component does not applied... no changes are visible

--- src/themes/custom/app/thumbnail/thumbnail.component.ts
+++ src/themes/custom/app/thumbnail/thumbnail.component.ts
@@ -3,10 +3,10 @@ import { ThumbnailComponent as BaseComponent } from '../../../../app/thumbnail/t
 
 @Component({
   selector: 'ds-thumbnail',
-  // styleUrls: ['./thumbnail.component.scss'],
-  styleUrls: ['../../../../app/thumbnail/thumbnail.component.scss'],
-  // templateUrl: './thumbnail.component.html',
-  templateUrl: '../../../../app/thumbnail/thumbnail.component.html',
+  styleUrls: ['./thumbnail.component.scss'],
+  //styleUrls: ['../../../../app/thumbnail/thumbnail.component.scss'],
+  templateUrl: './thumbnail.component.html',
+  //templateUrl: '../../../../app/thumbnail/thumbnail.component.html',
 })
 export class ThumbnailComponent extends BaseComponent {
 }
