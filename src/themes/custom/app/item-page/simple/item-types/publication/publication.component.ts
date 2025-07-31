d8a956b9b2752929a14c898adf413d9b0140019a
Repository Administrator <repozitar@techlib.cz>
2025-07-31 16:53:24 +0200

    refined licenses and added tooltips; ToDo: add NTK badge for No Licence case;

--- src/themes/custom/app/item-page/simple/item-types/publication/publication.component.ts
+++ src/themes/custom/app/item-page/simple/item-types/publication/publication.component.ts
@@ -12,7 +12,8 @@ import { PublicationComponent as BaseComponent } from '../../../../../../../app/
 @Component({
   selector: 'ds-publication',
   // styleUrls: ['./publication.component.scss'],
-  styleUrls: ['../../../../../../../app/item-page/simple/item-types/publication/publication.component.scss'],
+  styleUrls: ['../../../../../../../app/item-page/simple/item-types/publication/publication.component.scss',
+    './publication.component.scss'],
   templateUrl: './publication.component.html',
   //templateUrl: '../../../../../../../app/item-page/simple/item-types/publication/publication.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
