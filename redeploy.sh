2f37977e5c7519d6b8e111a944b26a12627a4005
Repository Administrator <repozitar@techlib.cz>
2025-09-02 16:05:08 +0200

    edit redeploy script so it automates yarn merge-i18n
    the script now merges files before redeployment and after building the image restores the src/assets/i18n files to the original state via git restore

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /opt/src-dspace/client/dspace-angular/
+yarn merge-i18n -s /opt/src-dspace/client/dspace-angular/src/themes/custom/assets/i18n/
 yarn build:prod
+git restore src/assets/i18n
 rm -rf /opt/dspace-angular/dist
 cp -r /opt/src-dspace/client/dspace-angular/dist /opt/dspace-angular/
 cd /opt/dspace-angular/
